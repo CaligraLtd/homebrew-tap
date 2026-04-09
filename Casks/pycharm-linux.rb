273133a2185be5adc0affa4f806dd6d8a360f16a
Max Haack <max@caligra.com>
2026-04-09 09:29:18 -0400

    feat(pycharm): add cask with auto-bump workflow
    Closes #3

---
+++ Casks/pycharm-linux.rb
@@ -0,0 +1,70 @@
+cask "pycharm-linux" do
+  arch intel: "",
+       arm:   "-aarch64"
+  os linux: "linux"
+
+  version "2026.1,261.22158.340"
+  sha256 arm64_linux:  "1f37a0a743245ba4bed8297cf1dc84db856bd1735a64c1c187f1282ed9b2e95c",
+         x86_64_linux: "6405985fe82bc6a419005388930cbe318b7419b82cb09c8494035a2be8511b0f"
+
+  url "https://download.jetbrains.com/python/pycharm-#{version.csv.first}#{arch}.tar.gz"
+  name "PyCharm"
+  desc "IDE for professional Python development"
+  homepage "https://www.jetbrains.com/pycharm/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=PCP&latest=true&type=release"
+    strategy :json do |json|
+      json["PCP"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
+      end
+    end
+  end
+
+  auto_updates false
+  conflicts_with cask: "jetbrains-toolbox-linux"
+
+  binary "pycharm-#{version.csv.first}/bin/pycharm"
+  artifact "jetbrains-pycharm.desktop",
+           target: "#{Dir.home}/.local/share/applications/jetbrains-pycharm.desktop"
+  artifact "pycharm-#{version.csv.first}/bin/pycharm.svg",
+           target: "#{Dir.home}/.local/share/icons/hicolor/scalable/apps/pycharm.svg"
+
+  preflight do
+    # Prevent PyCharm's built-in updater from conflicting with Homebrew
+    # TODO: enable server-side window decorations once JBR supports it properly
+    #   https://youtrack.jetbrains.com/issue/JBR-6187
+    File.write("#{staged_path}/pycharm-#{version.csv.first}/bin/pycharm64.vmoptions",
+               "-Dide.no.platform.update=true\n", mode: "a+")
+    FileUtils.mkdir_p("#{Dir.home}/.local/share/applications")
+    FileUtils.mkdir_p("#{Dir.home}/.local/share/icons/hicolor/scalable/apps")
+    File.write("#{staged_path}/jetbrains-pycharm.desktop", <<~EOS)
+      [Desktop Entry]
+      Version=1.0
+      Name=PyCharm
+      Comment=The Only Python IDE you need
+      Exec=#{HOMEBREW_PREFIX}/bin/pycharm %u
+      Icon=pycharm
+      Type=Application
+      Categories=Development;IDE;
+      Keywords=jetbrains;ide;python;
+      Terminal=false
+      StartupWMClass=jetbrains-pycharm
+      StartupNotify=true
+    EOS
+  end
+
+  postflight do
+    system "/usr/bin/xdg-icon-resource", "forceupdate"
+  end
+
+  zap trash: [
+    "#{Dir.home}/.cache/JetBrains/PyCharm#{version.major_minor}",
+    "#{Dir.home}/.config/JetBrains/PyCharm#{version.major_minor}",
+    "#{Dir.home}/.local/share/JetBrains/PyCharm#{version.major_minor}",
+  ]
+end
