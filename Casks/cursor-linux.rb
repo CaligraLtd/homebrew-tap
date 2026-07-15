5069b5a60eccc72b2f19107b86a3be5c9472c8d2
cholotes <cholotes@caligra.com>
2026-07-15 08:49:46 +0000

    chore(cursor): bump to 3.11.25

--- Casks/cursor-linux.rb
+++ Casks/cursor-linux.rb
@@ -3,9 +3,9 @@ cask "cursor-linux" do
   arch arm: "arm64", intel: "x64"
   file_arch = on_arch_conditional arm: "aarch64", intel: "x86_64"
 
-  version "3.11.19,bf249e6efb5b097f23d7e21d7283429f0760b74a"
-  sha256 arm64_linux:  "94500aeb14c80377749268f26c362c3136198436ebff16ac0c6fcdb99984f376",
-         x86_64_linux: "104e50b93335eac89be0c600933df83cb6c03978dbee171dc15b6b61210ab7f2"
+  version "3.11.25,fc2563ec93d793fc275eef734405a4fdf8b47b26"
+  sha256 arm64_linux:  "780ff30b9d4ea9bdd382e85536992c94804972138911ad24f1b1244bd925feb1",
+         x86_64_linux: "c68f422b4377cd5bf2f0d42dedbcf2bb32207cf7b2e9c9d8d048889d9e67ac30"
 
   url "https://downloads.cursor.com/production/#{version.csv.second}/linux/#{arch}/Cursor-#{version.csv.first}-#{file_arch}.AppImage",
       verified: "downloads.cursor.com/"
