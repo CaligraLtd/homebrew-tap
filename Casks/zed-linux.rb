6c6dc631c74db78479971edf8493dff47a32239e
cholotes <cholotes@caligra.com>
2026-08-06 08:55:53 +0000

    chore(zed): bump to 1.14.2

--- Casks/zed-linux.rb
+++ Casks/zed-linux.rb
@@ -1,9 +1,9 @@
 cask "zed-linux" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.13.2"
-  sha256 arm64_linux:  "708bf3642a86c1ac7367764b9b4bb32fc3c9bc18aafdf3b50e2b401f7df108db",
-         x86_64_linux: "44508886bed247c7544f64b66e06bd388ec3d406776b5457868bc14a67130724"
+  version "1.14.2"
+  sha256 arm64_linux:  "1c1d45a86a34301d1e9338ca344bfff9e3dea327a54ec24a61e74285172b1f78",
+         x86_64_linux: "6c476103b49a87dc62c46b8cc4dca84c0458ed53537de664ebdbc99b47daee2d"
 
   url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-#{arch}.tar.gz"
   name "Zed"
