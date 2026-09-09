c97bcf580ede4899a981f4b6d1fc088a7339134f
cholotes <cholotes@caligra.com>
2026-09-09 08:13:51 +0000

    chore(chrome): bump to 153.0.8010.36

--- Casks/google-chrome-linux.rb
+++ Casks/google-chrome-linux.rb
@@ -1,8 +1,8 @@
 require "etc"
 
 cask "google-chrome-linux" do
-  version "152.0.7977.82"
-  sha256 "22314c7d75648e12cfd94c6f5af3fa5e45d30a26de519f3086fca98ec0021a97"
+  version "153.0.8010.36"
+  sha256 "8af165edf32455880a4180932dc8081dba2221bf233869cf5c22f14b42db8ae5"
   os linux: "linux"
 
   url "https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{version}-1.x86_64.rpm"
